202ea2cb5b4d6310216b441628cd3813642eb979
Hanyoung Park <85710397+Hanyoung-Park@users.noreply.github.com>
2023-01-16 14:01:39 +0900

    Create 3003.cpp

---
+++ C++/3003.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int orig[6] = {1, 1, 2, 2, 2, 8};
+
+int main() {
+    int temp;
+    for(int i=0;i<6;i++) {
+        cin >> temp;
+        cout << orig[i] - temp << ' ';
+    }
+}
